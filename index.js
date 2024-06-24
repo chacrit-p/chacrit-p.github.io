38b1c67668c13f98aa593f6c595c50355ccc1453
chacrit-p <119285304+chacrit-p@users.noreply.github.com>
2024-06-25 02:21:14 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 const main = document.getElementById("main");
 const themeToggle = document.getElementById("theme-toggle");
+const title = document.getElementById("title");
 
 function setTheme(theme) {
   main.setAttribute("data-theme", theme);
   themeToggle.checked = theme === "dark";
 }
 
-window.onload = function () {
+window.onload = () => {
   const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    setTheme(savedTheme);
-  } else {
-    setTheme("dark");
-  }
+  setTheme(savedTheme ? savedTheme : "dark");
 };
 
-themeToggle.addEventListener("change", function () {
+themeToggle.addEventListener("change", () => {
   const theme = themeToggle.checked ? "dark" : "light";
   setTheme(theme);
   localStorage.setItem("theme", theme);
+});
+
+function typeText(element, text, speed = 100, deleteSpeed = 50, pause = 1000) {
+  let index = 0;
+  let isDeleting = false;
+
+  function type() {
+    if (!isDeleting && index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(type, speed);
+    } else if (isDeleting && index > 0) {
+      element.innerHTML = text.substring(0, index - 1);
+      index--;
+      setTimeout(type, deleteSpeed);
+    } else if (!isDeleting && index === text.length) {
+      setTimeout(() => {
+        isDeleting = true;
+        setTimeout(type, pause);
+      }, pause);
+    } else if (isDeleting && index === 0) {
+      isDeleting = false;
+      setTimeout(type, speed);
+    }
+  }
+
+  type();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const text = "Hi, Chacrit Poonpipit";
+  typeText(title, text);
 });
